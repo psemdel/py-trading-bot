50a32e1bcf72210163311022046037bc06c235ad
psemdel <maxime.delzenne@gmail.com>
2025-11-24 20:33:42 +0100

    kubernetes removed as docker is removed

--- py-trading-bot/kubernetes/sequence_start.sh
+++
@@ -1,7 +0,0 @@
-kubectl apply -f py-trading-bot/kubernetes/configmap.yml
-kubectl apply -f py-trading-bot/kubernetes/secret.yml
-kubectl apply -f py-trading-bot/kubernetes/redis.yml
-kubectl apply -f py-trading-bot/kubernetes/postgres.yml
-sleep 5
-kubectl apply -f py-trading-bot/kubernetes/django.yml
-kubectl apply -f py-trading-bot/kubernetes/celery.yml
